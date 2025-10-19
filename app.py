f17cd92d37b7ff9ced547aad94719713a01f8b8d
Angus Taylor <angustaylo99@gmail.com>
2025-10-19 13:35:51 +0100

    working on changing the database work to sqlite, trying to pass data to the index page.

--- app.py
+++ app.py
@@ -1,37 +1,6 @@
 from flask import Flask, render_template, session, request, redirect
-import sqlalchemy as db
+import sqlite3
 
-# This creates the engine object that acts as a central source
-# of connections for the database
-engine = db.create_engine('sqlite+pysqlite:///:memory:')
-connection = engine.connect()
-
-#  This creates the metadata object that contains
-#  the information about the tables and columns
-# that are contained in the database
-metadata = db.MetaData()
-
-
-# create the two tables with their columns
-tasks = db.Table(
-    'tasks',
-    metadata,
-    db.Column('id', db.Integer, primary_key=True, nullable=False, autoincrement=True),
-    db.Column('description', db.String(200)),
-    db.Column('subject', db.String(50)),
-    db.Column('subject_id', db.ForeignKey('subjects.id'), nullable=False)
-)
-subjects = db.Table(
-    'subjects',
-    metadata,
-    db.Column('id', db.Integer, primary_key=True, nullable=False, autoincrement=True),
-    db.Column('subjectName', db.String)
-)
-
-stmt = db.insert(subjects).values(subjectName='Miscellaneous')
-
-# This initializes and creates the tables into existence
-metadata.create_all(engine)
 
 app = Flask(__name__)
 
@@ -45,32 +14,43 @@ def apology(message):
 @app.route('/', methods =['GET', 'POST'])
 def index():
 
+    if request.method == 'GET':
+        # creates a connection to the database
+        con = sqlite3.connect('todosite.db')
+        
+        # returns the data in the form of dicts so column names can be accessed
+        # (otherwise it comes as a tuple that can't be accessed with column names)
+        con.row_factory =sqlite3.Row
+        
+        # Creates a cursor to manage the connections to the database
+        cur = con.cursor()
+
+        res = cur.execute(
+            'SELECT * FROM tasks;'
+        ).fetchall()
+
+        con.close()
+
+        tasks = []
+        for row in res:
+            tasks.append({
+                'description': res[length]['description'],
+                'subject': res[length]['subject'],
+                'deadline': res[length]['deadline']
+            })
+        return render_template('index.html', tasks=tasks)
+    
     if request.method == 'POST':
-        # get info from the table
-        description = request.form.get('description')
-        deadline = request.form.get('deadline')
-        subject = request.form.get('subject')
 
-        # create statement to get subject id for update.
-        get_sub_id = db.select(subjects).where(subjects.subjectName == subject)
-        subject_id = connection.execute(get_sub_id)
-
-        insert_task = db.insert(subjects).values(
-            description=description,
-            deadline=deadline,
-            subject=subject,
-            subject_id=subject_id
-        )
-        try:
-            connection.execute(insert_task)
-        except:
-            return "Coulnd't update the table"
-        return redirect('/')
+        return render_template('index.html', tasks=tasks)
 
 
-    get_task_info = db.select(tasks)
-    tasks = connection.execute(get_task_info)
 
-    return render_template('index.html', tasks=tasks)
+
+    return render_template('index.html')
+
+@app.route('/addSubject', methods=['GET', 'POST'])
+def addSubject():
+    return render_template('addSubject.html')
 
 
